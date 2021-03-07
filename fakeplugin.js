533110e9cfb6aa509762ffb1d98d3ee085c02286
Christian Dacanay <chrisdacanay1@gmail.com>
2021-03-07 17:10:42 +0000

    HOMEPAGE SLIDER -- ONGOING.

---
+++ fakeplugin.js
@@ -0,0 +1,3 @@
+function handleMobileClick(){
+    alert("cuntbag");
+}
--- js.js
+++ js.js
@@ -1,8 +1,30 @@
-var mobilBtn = document.querySelector(".mobile-menu-btn");
+(function(){
+    // ----- Mobile Button
+    var mobilBtn = document.querySelector(".mobile-menu-btn");
 
-function handleMobileClick(event) {
-    var navParent = document.querySelector(".navparent");
-    navParent.classList.toggle("navparent--open");
-}
+    function handleMobileClick(event) {
+        var navParent = document.querySelector(".navparent");
+        navParent.classList.toggle("navparent--open");
+    }
+    
+    mobilBtn.addEventListener("click", handleMobileClick);
 
-mobilBtn.addEventListener("click", handleMobileClick)
+
+    // ----- Slider
+	var slider = new Splide('.splide', {
+        pagination: false,
+        arrows: false
+    } );
+
+    slider.mount();
+
+    var icons = document.querySelectorAll(".headicons .icons");
+
+    icons.forEach((element, index) => {
+        element.addEventListener("click", function handleIconClick() {
+            slider.go(index);
+        });
+    });
+
+
+})();
